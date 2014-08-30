1e7b786be107aa3d466d732a6dd86c3848c32922
kenneth-hao <drmrontheway@gmail.com>
2014-08-30 16:28:57 +0800

    base project

---
+++ src/main/java/org/study/simpleweb/web/UserController.java
@@ -0,0 +1,7 @@
+package org.study.simpleweb.web;
+
+/**
+ * Created by haoyuewen on 8/30/14.
+ */
+public class UserController {
+}
---
+++ src/main/webapp/WEB-INF/errorpage/404.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<p>This is My First Maven Webapp In IntelliJ IDEA!</p>
+</body>
+</html>
